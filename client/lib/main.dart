26a2e8d10cbe8e222d1d95f92215ae199c2dccea
Afiq Amri <120144945+afiqamrii@users.noreply.github.com>
2025-01-12 10:26:37 +0800

    adjusting on the bottom nav bar

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'lecturer_pov/lecturer_homepage.dart';
+import 'package:smartclass_fyp_2024/lecturer_pov/template/lecturer_bottom_navbar.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -13,13 +13,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'SmartClass FYP',
       theme: ThemeData(
         fontFamily: 'Figtree',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LectHomepage(),
+      home:
+          const MainScreen(), // Navigate to the MainScreen with navigation functionality
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final int _currentIndex = 0;
+  // final List<Widget> _screens = [
+  //   LectHomepage(), // Lecturer homepage
+  //   const LecturerMyclass(), // MyClass screen
+  //   const LecturerProfilePage(), // Lecturer profile page
+  // ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LectBottomNavBar(initialIndex: _currentIndex),
     );
   }
 }
