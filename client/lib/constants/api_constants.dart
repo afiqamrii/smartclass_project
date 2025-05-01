5212f37e1fb8107dff47a859dde692bdfc4f2ef1
Afiq Amri <120144945+afiqamrii@users.noreply.github.com>
2025-05-01 16:27:31 +0800

    feat: Update API base URL and add Report Utility page

--- client/lib/constants/api_constants.dart
+++ client/lib/constants/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
-  static const baseUrl = "http://192.168.0.99:3000";
-  // static const baseUrl = "http://172.20.10.2:3000";
+  // static const baseUrl = "http://192.168.0.99:3000";
+  static const baseUrl = "http://172.20.10.2:3000";
 }
