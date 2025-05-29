6d852c8b895f432a8ef228c82e3fe52d4e45ce04
Afiq Amri <120144945+afiqamrii@users.noreply.github.com>
2025-05-29 22:40:44 +0800

    fix: Update API constants to use the correct base and socket URLs for the current environment

--- client/lib/constants/api_constants.dart
+++ client/lib/constants/api_constants.dart
@@ -1,8 +1,10 @@
 class ApiConstants {
-  static const baseUrl = "http://192.168.0.99:3000";
-  // static const baseUrl = "http://172.20.10.2:3000";
+  // static const baseUrl = "http://192.168.0.99:3000";
+  static const baseUrl = "http://172.20.10.2:3000";
 
   //Base url for websocket server
-  static const socketUrl = "ws://192.168.0.99:3000";
+  // static const socketUrl = "ws://192.168.0.99:3000";
 
+  //Base url for websocket server PHONE HOTSPOT
+  static const socketUrl = "ws://172.20.10.2:3000";
 }
